bd07243b75105c8941201f8b51c6fe08d451a3aa
cb@ndy <gektor93@gmail.com>
2022-05-23 13:04:49 +0300

    bootstrap-sass-starterPack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import './main.scss';
+var reqScripts = require.context("./scripts", true, /^(.*\.(js$))[^.]*$/i);
+reqScripts.keys().forEach(reqScripts);
